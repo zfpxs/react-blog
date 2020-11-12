67c0af3d1db43f427e738d77cd8e4c03d6051416
zfpxs <535035192@qq.com>
2020-11-12 19:10:37 +0800

    1

---
+++ qqd-server/config/plugin.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// exports.nunjucks = {
+//   enable: true,
+//   package: 'egg-view-nunjucks',
+// };
+exports.cors = {
+  enable: true,
+  package: 'egg-cors'
+} 
+exports.mysql = {
+  enable: true,
+  package: 'egg-mysql'
+}
